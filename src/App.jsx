1b79e1b83a87cf446edb7b016a644bd5de1b6bd7
Max Sinyakov <max@sinyakov.com>
2018-03-21 20:25:39 +0300

    add input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Input from './components/Input';
 import Button from './components/Button';
 
 class App extends React.Component {
-  state = {};
+  state = {
+    top: '',
+    bottom: '',
+  };
+
+  onChange = inputType => (event) => {
+    this.setState({ [inputType]: event.target.value });
+  };
 
   render() {
+    const { top, bottom } = this.state;
     return (
       <div className="content">
+        <Input value={top} placeholder="Top" onChange={this.onChange('top')} />
+        <Input value={bottom} placeholder="Bottom" onChange={this.onChange('bottom')} />
         <Button>Go</Button>
       </div>
     );
