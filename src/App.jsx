af07175ecb75b4766be4c17755bb112bc6f18249
Max Sinyakov <max@sinyakov.com>
2018-03-21 20:32:39 +0300

    use enum, add onSubmit handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,43 @@ import React from 'react';
 import Input from './components/Input';
 import Button from './components/Button';
 
+const INPUT_TYPE = Object.freeze({
+  TOP: 'top',
+  BOTTOM: 'bottom',
+});
+
+const isValidHex = value => value.match(/#[0-9A-Fa-f]{6}$/g);
+
 class App extends React.Component {
   state = {
-    top: '',
-    bottom: '',
+    [INPUT_TYPE.TOP]: '',
+    [INPUT_TYPE.BOTTOM]: '',
+    background: '#eee',
   };
 
   onChange = inputType => (event) => {
     this.setState({ [inputType]: event.target.value });
   };
 
-  render() {
+  onSubmit = (event) => {
+    event.preventDefault();
     const { top, bottom } = this.state;
+
+    if (isValidHex(top) && isValidHex(bottom)) {
+      const background = `linear-gradient(${top}, ${bottom})`;
+      this.setState({ background });
+    }
+  };
+
+  render() {
+    const { top, bottom, background } = this.state;
     return (
-      <div className="content">
-        <Input value={top} placeholder="Top" onChange={this.onChange('top')} />
-        <Input value={bottom} placeholder="Bottom" onChange={this.onChange('bottom')} />
-        <Button>Go</Button>
+      <div className="content" style={{ background }}>
+        <form onSubmit={this.onSubmit}>
+          <Input value={top} placeholder="Top" onChange={this.onChange(INPUT_TYPE.TOP)} />
+          <Input value={bottom} placeholder="Bottom" onChange={this.onChange(INPUT_TYPE.BOTTOM)} />
+          <Button>Go</Button>
+        </form>
       </div>
     );
   }
